df76a360456851224bedb54946bafed77b409236
kiran1929 <gudepukirandeep@gmail.com>
2026-05-15 10:22:20 +0530

    fixed wrong backend url in config.js

--- frontend/config.js
+++ frontend/config.js
@@ -2,7 +2,7 @@
 // Centralized configuration for the WishWall project
 
 const CONFIG = {
-    API_URL: "https://garuda-wishwall-backend.onrender.com", // Production URL
+    API_URL: "https://garuda-wish-wall-latest.onrender.com", // Production URL
     //API_URL: "http://localhost:3000", // Local development URL
 };
 
